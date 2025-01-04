356d00da519d898517b71da883ad3cb171b1108a
Vilaisak SOPHABMIXAY <vilaisak.s@phonesackgroup.com>
2025-01-04 11:48:19 +0700

    VDO 02

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h1>App - Vue 2025</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minipos</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>Hello Laravel</h1>
+    <hr>
+    <div id="app-vue"></div>
+
+    @vite('resources/js/app.js')
+</body>
+</html>
